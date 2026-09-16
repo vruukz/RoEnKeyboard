befd6870bff5e65c17fad49141ec279bf3835b87
Carpinisan Andrei <smellylawyer84@gmail.com>
2026-09-16 08:18:05 +0300

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.andrei.roenkeyboard"
         minSdk = 23
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
